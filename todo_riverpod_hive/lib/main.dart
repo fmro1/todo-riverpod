18de04a677b3190fce2085c5e3a263a276bb069a
fmro1 <felipe.moriya@hotmail.com>
2025-08-19 16:20:11 -0300

    usando hive as db

---
+++ todo_riverpod_hive/lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+
+import 'pages/providers/theme/theme_provider.dart';
+import 'pages/providers/theme/theme_state.dart';
+import 'pages/todos_page.dart';
+import 'repositories/hive_todos_repository.dart';
+import 'repositories/providers/todos_repository_provider.dart';
+
+void main() async {
+  Hive.initFlutter();
+  Hive.openBox("todos");
+  runApp(
+    ProviderScope(
+      overrides: [
+        todosRepositoryProvider.overrideWithValue(HiveTodosRepository())
+      ],
+      child: MyApp(),
+    ),
+  );
+}
+
+class MyApp extends ConsumerWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final currentTheme = ref.watch(themeProvider);
+
+    return MaterialApp(
+      title: 'Todos AsyncValue',
+      debugShowCheckedModeBanner: false,
+      theme: switch (currentTheme) {
+        LightTheme() => ThemeData.light(useMaterial3: true),
+        DarkTheme() => ThemeData.dark(useMaterial3: true),
+      },
+      home: const TodosPage(),
+    );
+  }
+}
